ef9ab751ae06f66df816943ea10fdfe741a8e402
Shah jalal <rjshahjalal132@gmail.com>
2023-12-17 12:02:44 +0600

    conditionaly table crate done . but data not showing

--- imjol-contact-form.php
+++ imjol-contact-form.php
@@ -65,6 +65,99 @@ function imjol_database_design() {
 }
 register_activation_hook( __FILE__, 'imjol_database_design' );
 
+// Register Top Label Menu
+add_action( 'admin_menu', 'show_all_user_infos' );
+function show_all_user_infos() {
+    add_menu_page(
+        'all_users_infos',
+        'All Users Infos',
+        'manage_options',
+        'all_users_infos',
+        'show_all_users_infos_html',
+        'dashicons-admin-users',
+        20
+    );
+
+    function show_all_users_infos_html() {
+        ?>
+            <div id="user-infos-table">
+            <?php 
+                            
+                    // Retrieve Data From Database
+                    $imjol_user_data = new WP_Query([
+                        'table_name' => 'wp_imjol_forms',
+                        'fields' => ['user_id', 'first_name', 'address', 'email', 'phone', 'whatsapp', 'mobile_app', 'website', 'software', 'requirement', 'budget', 'deadline'],
+                        'order_by' => 'user_id',
+                        'order' => 'ASC',
+                    ]);
+
+                    // Conditionally table create
+                    if($imjol_user_data->have_posts()) {
+                        ?>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>User ID</th>
+                                        <th>User Name</th>
+                                        <th>Address</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>What's App</th>
+                                        <th>Website</th>
+                                        <th>Software</th>
+                                        <th>Requirements</th>
+                                        <th>Budget</th>
+                                        <th>Deadline</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <?php 
+                                            while($imjol_user_data->have_posts()) : $imjol_user_data->the_post();
+
+                                            // get table field data
+                                            $user_id = $imjol_user_data->user_id;
+                                            $first_name = $imjol_user_data->first_name;
+                                            $address = $imjol_user_data->address;
+                                            $email = $imjol_user_data->email;
+                                            $phone = $imjol_user_data->phone;
+                                            $whatsapp = $imjol_user_data->whatsapp;
+                                            $mobile_app = $imjol_user_data->mobile_app;
+                                            $website = $imjol_user_data->website;
+                                            $software = $imjol_user_data->software;
+                                            $requirement = $imjol_user_data->requirement;
+                                            $budget = $imjol_user_data->budget;
+                                            $deadline = $imjol_user_data->deadline;
+                                        ?>
+                                                <td> <?php echo $user_id; ?> </td>
+                                                <td> <?php echo $first_name; ?> </td>
+                                                <td> <?php echo $address; ?> </td>
+                                                <td> <?php echo $email; ?> </td>
+                                                <td> <?php echo $phone; ?> </td>
+                                                <td> <?php echo $whatsapp; ?> </td>
+                                                <td> <?php echo $mobile_app; ?> </td>
+                                                <td> <?php echo $website; ?> </td>
+                                                <td> <?php echo $software; ?> </td>
+                                                <td> <?php echo $requirement; ?> </td>
+                                                <td> <?php echo $budget; ?> </td>
+                                                <td> <?php echo $deadline; ?> </td>
+                                            <?php
+                                            endwhile;
+                                        ?>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        <?php                    
+                    }else{
+                        echo 'No Data Found';
+                    }
+
+                ?>
+            </div>
+        <?php
+    }
+}
+
 // Requiring plugin files
 require_once IMJOL_PLUGIN_PATH . '/inc/form_shortcode.php';
 require_once IMJOL_PLUGIN_PATH . '/inc/enqueue_assets.php';
